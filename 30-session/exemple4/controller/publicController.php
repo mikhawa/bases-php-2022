c64f1415692f8825033b50ba4d448259c3d9424a
mikhawa <michaeljpitz@gmail.com>
2023-02-24 16:10:42 +0100

    access private run

--- 30-session/exemple4/controller/publicController.php
+++ 30-session/exemple4/controller/publicController.php
@@ -2,4 +2,29 @@
 //echo __FILE__;
 # Contrôleur public
 
+// si le formulaire a été envoyé
+if(isset($_POST['username'],$_POST['password'])){
+
+    // protection des données utilisateurs
+    $username = htmlspecialchars(strip_tags(trim($_POST['username'])),ENT_QUOTES);
+    $password = htmlspecialchars(strip_tags(trim($_POST['password'])),ENT_QUOTES);
+
+    // is_connected contient l'état de la connexion
+    $is_connect = connectUser($db,$username,$password);
+
+    // si c'est un chaîne de caractère
+    if (is_string($is_connect)) {
+        $erreur = $is_connect;
+    }
+
+    // redirection si connexion ok
+    if ($is_connect===true) {
+        // redirection sur index.php
+        header("Location: ./");
+        exit();
+    }
+    
+
+}
+
 include_once "../view/publicView.php";
--- 30-session/exemple4/model/UserModel.php
+++ 30-session/exemple4/model/UserModel.php
@@ -1,5 +1,50 @@
 <?php
 
 // connexion d'un user
+function connectUser(mysqli $myDB, string $uname, string $upwd): string|bool {
+
+    // traitement du username pour éviter un injection sql
+    // lié à la connexion mysqli (jeux de caractères)
+    $uname = mysqli_real_escape_string($myDB,$uname);
+
+    // sql - récupération via le username uniquement
+    $sql="SELECT * FROM user WHERE username='$uname';";
+
+    try {
+        // exécution de la requête
+        $query = mysqli_query($myDB, $sql);
+        
+    }catch(Exception $e){
+
+        // envoie de texte en cas d'erreur
+        return $e->getMessage();
+    }
+    
+    // si on a pas récupéré d'utilisateur on quitte en envoyant un message
+    if(mysqli_num_rows($query)==0) return "Login ou mot de passe incorrecte";
+
+    // conversion de l'utilisateur en tableau associatif
+    $recup = mysqli_fetch_assoc($query);
+
+    // vérification du mot de passe tapé = DB (crypté)
+    if (password_verify($upwd, $recup['password'])) {
+
+        // création de session avec le contenu de notre requête SQL
+        $_SESSION = $recup;
+        // suppresion du mot de passe et de l'id unique
+        unset($_SESSION['password'],$_SESSION['uniqid']);
+        // création de notre variable prouvant la validité de la session
+        $_SESSION['monId'] = session_id();
+
+        // on envoie vraie si la connexion est une réussite
+        return true;
+
+    }else{
+        return "Login ou mot de passe incorrecte";
+    }
+    
+    
+
+}
 
 // déconnexion d'un user
